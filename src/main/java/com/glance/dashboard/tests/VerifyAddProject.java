4da0e15a269007d2a28267da475c815a271b89da
tsajeththa <sajeththa@gmail.com>
2016-12-07 16:31:26 +0530

    Commit test cases

--- src/main/java/com/glance/dashboard/tests/VerifyAddProject.java
+++ src/main/java/com/glance/dashboard/tests/VerifyAddProject.java
@@ -1,6 +1,7 @@
 package com.glance.dashboard.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.glance.common.tests.BaseTest;
@@ -22,7 +23,90 @@ public class VerifyAddProject extends BaseTest {
 	AddProjectPage addProject;
 	DashboardPage dashboard;
 	
+	@BeforeMethod
+	public void login() {
+	// Login to Glance By entering user name and password
+	login=new LoginPage(driver);
+	login.enterUsername(userName);
+	login.enterPassword(password);
+	login.clickLoginBtn();
+	
+	}
 	@Test (priority=0)
+	public void GL_Settings_AP_15_verifyAddProjectNavigation() throws Exception{
+		
+		
+		settingPage=new SettingsPage(driver);
+		leftPanel=new CommonPageLeftPane(driver);
+		
+		// click on settings button in left panal
+		leftPanel=new CommonPageLeftPane(driver);
+		leftPanel.clickSettings();
+		Assert.assertTrue(settingPage.verifyNavigationToSettingPage("Dashboard Management"
+));
+				
+		
+		//click on add new project from left panal in array
+		leftPanel.clickOnAddNewProjectLink();
+		addProject=new AddProjectPage(driver);
+		
+		// check whether navigated to Add Project page
+		Assert.assertTrue(addProject.verifyNavigationToAddProjectPage("Project "));
+		System.out.println("Navigation done through left panal");
+		
+		
+	}
+	
+	@Test (priority=1)
+	public void GL_Settings_AP_16_verifyAddProject() throws Exception{
+	
+				// click on settings button in left panal
+				leftPanel=new CommonPageLeftPane(driver);
+				leftPanel.clickSettings();
+				
+				
+				commonPage = new CommonPageObject(driver);
+				commonPage.waitForPageLoad(10);
+				
+				// Get the count of Number of Projects before adding a project and click on add project button
+				settingPage=new SettingsPage(driver);
+				int previousCountProject=settingPage.getCountAddProject();
+				System.out.println("Previous Count:"+previousCountProject);
+				commonPage.waitForPageLoad(10);
+				settingPage.clickAddProjectBtn();
+				commonPage.waitForPageLoad(10);
+				
+				//Add Project Page
+				
+				addProject=new AddProjectPage(driver);
+				
+				// check whether navigated to Add Project page
+				Assert.assertTrue(addProject.verifyNavigationToAddProjectPage("Project "));
+				
+				
+		        projectName=RandomNameGenerator.getName();
+		        System.out.println(projectName);
+				addProject.enterProjectName(projectName);
+				addProject.enterClientProjectId(clientProjectId);
+				addProject.selectEngagementName(engagementName);
+				addProject.selectApplicationName(applicationName);
+				System.out.println("=============================================");
+				addProject.selectFundingStartDate(fundStartDate);
+				addProject.selectFundingEndDate(fundEndDate);
+				addProject.selectFundingReleaseDate(fundReleaseDate);
+				addProject.selectStatus(status);
+				
+				//Click on submit button
+				addProject.clickSubmitBtn();
+				commonPage.waitForPageLoad(30);
+				addProject.clickAddNewBtn();
+	
+		
+	}
+	
+	
+	
+	/*@Test (priority=0)
 	public void verifyAddProject() throws Exception {
 		
 		
@@ -260,7 +344,7 @@ public class VerifyAddProject extends BaseTest {
 		
 	
 	
-	}
+	}*/
 
 	
 	
