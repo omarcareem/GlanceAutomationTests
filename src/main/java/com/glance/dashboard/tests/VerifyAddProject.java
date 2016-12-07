b6661640a4da16fc6ea860ad0d0a4edd9da78784
tsajeththa <sajeththa@gmail.com>
2016-12-07 17:12:37 +0530

    commit sajeththa

--- src/main/java/com/glance/dashboard/tests/VerifyAddProject.java
+++ src/main/java/com/glance/dashboard/tests/VerifyAddProject.java
@@ -57,7 +57,7 @@ public class VerifyAddProject extends BaseTest {
 		
 	}
 	
-	@Test (priority=1)
+/*	@Test (priority=1)
 	public void GL_Settings_AP_16_verifyAddProject() throws Exception{
 	
 				// click on settings button in left panal
@@ -102,7 +102,219 @@ public class VerifyAddProject extends BaseTest {
 				addProject.clickAddNewBtn();
 	
 		
+	}*/
+	
+/*	@Test (priority=2)
+	public void GL_Settings_AP_17_verifyAddProjectSubmission() throws Exception{
+	
+				// click on settings button in left panal
+				leftPanel=new CommonPageLeftPane(driver);
+				leftPanel.clickSettings();
+				
+				
+				commonPage = new CommonPageObject(driver);
+				commonPage.waitForPageLoad(10);
+				
+				//click on add project button
+				settingPage=new SettingsPage(driver);
+				settingPage.clickAddProjectBtn();
+				commonPage.waitForPageLoad(10);
+				
+				//Add Project Page
+				
+				addProject=new AddProjectPage(driver);
+				
+				// check whether navigated to Add Project page
+				Assert.assertTrue(addProject.verifyNavigationToAddProjectPage("Project "));
+				
+				
+		        projectName=RandomNameGenerator.getName();
+		        System.out.println(projectName);
+				addProject.enterProjectName(projectName);
+				addProject.enterClientProjectId(clientProjectId);
+				addProject.selectEngagementName(engagementName);
+				addProject.selectApplicationName(applicationName);
+				System.out.println("=============================================");
+				addProject.selectFundingStartDate(fundStartDate);
+				addProject.selectFundingEndDate(fundEndDate);
+				addProject.selectFundingReleaseDate(fundReleaseDate);
+				addProject.selectStatus(status);
+				
+				//Click on submit button
+				addProject.clickSubmitBtn();
+				System.out.println("New Project has been added when clicking submit button");
+				commonPage.waitForPageLoad(30);
+				//click on dashboard button
+				addProject.clickGoToDashBoardBtn();
+				commonPage.waitForPageLoad(30);
+				
+				dashboard =new DashboardPage(driver);
+				// check whether navigated to dashboard page
+				Assert.assertTrue(dashboard.verifyNavigationToDashboardPage("Account Level Dashboard"));
+	
+		
+	}*/
+	
+/*	@Test (priority=3)
+	public void GL_Settings_AP_18_verifyAddedProjectValidation() throws Exception{
+	
+		// click on settings button in left panal
+		leftPanel=new CommonPageLeftPane(driver);
+		leftPanel.clickSettings();
+		
+		
+		commonPage = new CommonPageObject(driver);
+		commonPage.waitForPageLoad(10);
+		
+		// Get the count of Number of Projects before adding a project and click on add project button
+		settingPage=new SettingsPage(driver);
+		int previousCountProject=settingPage.getCountAddProject();
+		System.out.println("Previous Count:"+previousCountProject);
+		commonPage.waitForPageLoad(10);
+		settingPage.clickAddProjectBtn();
+		commonPage.waitForPageLoad(10);
+		
+		//Add Project Page
+		
+		addProject=new AddProjectPage(driver);
+		
+		// check whether navigated to Add Project page
+		Assert.assertTrue(addProject.verifyNavigationToAddProjectPage("Project "));
+		
+		
+        projectName=RandomNameGenerator.getName();
+        System.out.println(projectName);
+		addProject.enterProjectName(projectName);
+		addProject.enterClientProjectId(clientProjectId);
+		addProject.selectEngagementName(engagementName);
+		addProject.selectApplicationName(applicationName);
+		System.out.println("=============================================");
+		addProject.selectFundingStartDate(fundStartDate);
+		addProject.selectFundingEndDate(fundEndDate);
+		addProject.selectFundingReleaseDate(fundReleaseDate);
+		addProject.selectStatus(status);
+		
+		//Click on submit button
+		addProject.clickSubmitBtn();
+		commonPage.waitForPageLoad(30);
+		
+		//click on dashboard button
+		addProject.clickGoToDashBoardBtn();
+		commonPage.waitForPageLoad(30);
+		
+		dashboard =new DashboardPage(driver);
+		// check whether navigated to dashboard page
+		Assert.assertTrue(dashboard.verifyNavigationToDashboardPage("Account Level Dashboard"));
+		
+		//click on settings page
+		leftPanel.clickSettings();
+		Assert.assertTrue(settingPage.verifyNavigationToSettingPage("Dashboard Management"
+));
+		
+		//get current count of number of accounts 
+		int newestCountProject=settingPage.getCountAddProject();
+        System.out.println(newestCountProject);
+        
+        //compare the counts 
+        if(newestCountProject > previousCountProject)
+               System.out.println("New Project has been added");
+        
+        else 
+               System.out.println("Project has not been added");     
+        commonPage.waitForPageLoad(10);
+        
+	}*/
+	/*
+	@Test (priority=4)
+	public void GL_Settings_AP_19_verifyCancelAddProject() throws Exception{
+		// click on settings button in left panal
+		leftPanel=new CommonPageLeftPane(driver);
+		leftPanel.clickSettings();
+		Assert.assertTrue(settingPage.verifyNavigationToSettingPage("Dashboard Management"
+));
+		
+		commonPage = new CommonPageObject(driver);
+					
+		// Get the count of Number of Projects before adding a project and click on add project button
+		settingPage=new SettingsPage(driver);
+		int previousCountProject=settingPage.getCountAddProject();
+		System.out.println("Previous Count:"+previousCountProject);
+		commonPage.waitForPageLoad(10);
+		settingPage.clickAddProjectBtn();
+		commonPage.waitForPageLoad(10);
+		
+		
+		//Add Project Page
+		
+		addProject=new AddProjectPage(driver);
+		
+		// check whether navigated to Add Project page
+		Assert.assertTrue(addProject.verifyNavigationToAddProjectPage("Project "));
+
+		
+        projectName=RandomNameGenerator.getName();
+        System.out.println(projectName);
+		addProject.enterProjectName(projectName);
+		addProject.enterClientProjectId(clientProjectId);
+		addProject.selectEngagementName(engagementName);
+		addProject.selectApplicationName(applicationName);
+		System.out.println("=============================================");
+		addProject.selectFundingStartDate(fundStartDate);
+		addProject.selectFundingEndDate(fundEndDate);
+		addProject.selectFundingReleaseDate(fundReleaseDate);
+		addProject.selectStatus(status);
+		
+		
+		//Click on Cancel button
+		addProject.clickCancelBtn();
+		commonPage.waitForPageLoad(10);
+		Assert.assertTrue(settingPage.verifyNavigationToSettingPage("Dashboard Management"
+));
+		//get current count of number of accounts 
+        int newestCountProject=settingPage.getCountAddProject();
+        System.out.println(newestCountProject);
+
+        //compare the counts 
+        if(newestCountProject > previousCountProject)
+               System.out.println("New Project has been added");
+        
+        else 
+               System.out.println("Project has not been added");     
+        commonPage.waitForPageLoad(10);
+		
+		
+		
+	}*/
+	
+	
+	/*@Test (priority=5)
+	public void GL_Settings_AP_20_verifyCloseCurrentPane() throws Exception{
+        //click on add new on left pane
+        leftPane.clickOnAddNewAccountLink();
+        
+        //create an account
+        addAccount=new AddAccountPage(driver);
+        Assert.assertTrue(addAccount.verifyNavigationToAddAccountPage("Account"));
+        addAccount.clickClose();
+
+		
 	}
+	
+	@Test (priority=6)
+	public void GL_Settings_AP_21_verifyCollapseCurrentPane() throws Exception{
+		
+		//click on add new on left pane
+        leftPane.clickOnAddNewAccountLink();
+        
+        //create an account
+        addAccount=new AddAccountPage(driver);
+        Assert.assertTrue(addAccount.verifyNavigationToAddAccountPage("Account"));
+        addAccount.clickCollapse();
+
+	}*/
+	
+	
+	
 	
 	
 	
